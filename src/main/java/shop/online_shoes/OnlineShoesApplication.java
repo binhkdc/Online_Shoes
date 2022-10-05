e138b29eb9caa4edd8cf6d113e71004b16d6b336
binhkdc <ledangbinh18122002@gmail.com>
2022-10-05 13:14:29 +0700

    first commit Signed-off-by: binhkdc <ledangbinh18122002@gmail.com>

---
+++ src/main/java/shop/online_shoes/OnlineShoesApplication.java
@@ -0,0 +1,13 @@
+package shop.online_shoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/online_shoes/OnlineShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.online_shoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
